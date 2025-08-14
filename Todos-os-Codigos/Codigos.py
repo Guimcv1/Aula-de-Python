7561bfc06f8f63643feceb5f8b458e95f16776e1
Guilherme Martins <guimcvilmar@gmail.com>
2025-08-13 23:16:47 -0300

    Sistema de codigos

---
+++ Todos-os-Codigos/Codigos.py
@@ -0,0 +1,96 @@
+#------------------------- Este é um Repositorio de Códigos Python-------------------------
+
+#*********************************** Codigos Simples ***********************************
+
+#----------------------------------- Códigos de Debug ----------------------------------- 
+
+# Codigo para execultar no terminal
+'''
+python nome_do_arquivo.py
+
+'''
+#---------------------------- Codigo para gerenciar as pastas ----------------------------
+# Abrir uma pasta
+'''
+cd Nome_da_pasta
+
+'''
+# Voltar uma pasta
+'''
+cd ..
+
+'''
+
+
+#**************************************** Prints ****************************************
+
+# print() - imprime algo no terminal podendo ser uma variavel ou um texto
+
+print()
+
+# Se colocar '' ou "" você pode imprimir um texto
+
+print("Olá mundo!")
+
+# Usando Varíavel 
+
+Joao:str = "Olá Mundo!"
+print(Joao)
+
+# Usando Texto com variavel - colocar a virgula colocara um espaço entre elas automaticamente
+
+Joao:str = "Olá Mundo!"
+print(Joao, "Olá mundo 2.0")
+
+# Usando Texto com variavel com substituição da virgula 
+# Ele ira substituir a virgula com qualquer coisa dentro do sep=
+
+Joao:str = "Olá Mundo!"
+print(Joao, "Olá mundo 2.0",sep=" /// ")
+
+# Usando o f
+
+juntas="Conectadas"
+print(f"Posso colocar texto e variavel {juntas} sem o uso de virgulas")
+
+# Imprimir o tipo de variavel - Usando o codigo type é possivel verificar 
+# qual é o tipo de variavel dentro dos parenteses
+
+inteiro = 10
+print(type(inteiro))
+
+# inputs com print e definição de tipo
+
+Variavel:int=input("Digite um Numero: ")
+
+#---------------------------------------------------- Variaveis ----------------------------------------------------
+
+# Inteira - Números inteiros
+
+Num:int
+Num=12
+
+# Texto - String 
+
+Texto:str
+Texto="Opá pessoal"
+
+# Numeros quebrados - float
+
+Num_quebrado:float
+Num_quebrado=14.2566
+
+# Verdadeiro ou falso - bool
+# Sera Verdadeiro ou falso dependendo se tem valor atribuido a ele, se tiver texto ou numero ele sera verdadeiro
+
+sera:bool
+sera="1567856897"
+print(sera)
+
+# Vai dar verdadeiro
+
+
+
+
+
+
