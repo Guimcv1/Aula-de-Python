fd374945aa41450b308091a4c4bb0fd4f0a79980
Guimcv <guimcvilmar@gmail.com>
2025-09-03 22:46:26 -0300

    main

--- Todos-os-Codigos/Testes/teste6.py
+++ Todos-os-Codigos/Testes/teste6.py
@@ -1,62 +1,43 @@
-from os import system as console
-import json
-def teste1():
-    input('')
+import os
+limpar = lambda: os.system('cls' if os.name == 'nt' else 'clear')
 
+def crip():
+    limpar()
+    alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','ç','$','!','@','#','%']
+    numeros = [i for i in range(36,80)]
+    lista = []
+    lista2 = []
+    numeros.reverse()
+    var = input('Digite algo: ')
 
+    for i in var:
+        if i in alfabeto:
+            lista = alfabeto.index(i)
+            lista2.append(numeros[lista])
+    print(*lista2, sep='')
 
-# Uma tabela de quatro colunas/quatro linhas ‒ uma matriz bidimensional (4x4)
- 
-table = [[":(", ":)", ":(", ":)"],
-         [":)", ":(", ":)", ":)"],
-         [":(", ":)", ":)", ":("],
-         [":)", ":)", ":)", ":("]]
- 
-print(table)
-print(table[0][0])  # outputs: ':('
-print(table[0][3])  # outputs: ':)'
+def des():
+    lista2 = []
+    alfabeto =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','ç','$','!','@','#','%']
+    numero = [str(i) for i in range(36,80)]
+    print(numero)
+    conplat =[]
+    while True:
+        conp = input('Digite o arquivo criptografado\n')
+        if conp == "":
+            break
+        else:
+            conplat.append(conp)
 
+    #numero.reverse()
 
-
-
-
-
-
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-to_find = 5
-found = False
- 
-for i in range(len(my_list)):
-    found = my_list[i] == to_find
-    if found:
-        break
- 
-if found:
-    print("Elemento encontrado no índice", i)
-else:
-    print("ausente")
- 
-
-my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
-my_list.sort()
-i = True
-lista=[]
-print("A lista com os elementos exclusivos aqui")
-while i == True:
-    if my_list[0] == my_list[1]:
-        del my_list[1]
-        lista = my_list.pop(0)
-    else:
-        i = False
-        continue
-for i in range(lista):
-    print(i)
-
-
-def introduction(first_name, last_name):
-    print("Olá meu nome é", first_name, last_name)
- 
-introduction("Skywalker", "Luke")
-introduction("Quick", "Jesse")
-introduction("Kent", "Clark")
- 
+    for i in conplat: 
+        if i in numero:
+            numero.reverse()
+            lista = numero.index(i)
+            lista2.append(alfabeto[lista])
+            
+    print(*lista2)
+crip()
+des()
+    
